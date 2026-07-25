81958dfdd2f89155ac660e8ee84154ad4b2efe12
Juha Ala-Rantala <juha@ala-ranta.la>
2026-07-25 22:29:11 +0300

    CCG

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -39,7 +39,7 @@ const NAVIGATION_LINKS = [
   { href: "/livestreams", labelKey: "livestreams" },
   { href: "/events", labelKey: "latestEvents" },
   { href: "/pickems", labelKey: "pickems" },
-  { href: "/fun/ccg", labelKey: "ccg" },
+  // { href: "/fun/ccg", labelKey: "ccg" },
 ] as const;
 
 const getDesktopNavLinkClass = (isCurrent: boolean, previousIsCurrent: boolean, nextIsCurrent: boolean) =>
